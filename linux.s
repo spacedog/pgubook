0d4ac1ab3b97e7024fe53ec3719d089a584f95a7
anton baranov <abaranov@linux.com>
2014-05-09 11:53:21 +0000

    linux.s: common linux constants

---
+++ linux.s
@@ -0,0 +1,20 @@
+#
+## Common Linux Definition
+#
+
+## Word size
+.equ WORD_SIZE,       8
+## Syscalls
+.equ SYS_READ,        0
+.equ SYS_WRITE,       1
+.equ SYS_OPEN,        2
+.equ SYS_CLOSE,       3
+.equ SYS_BRK,         12
+.equ SYS_EXIT,        60
+## Standard file  descriptors
+.equ STDIN,           0
+.equ STDOUT,          1
+.equ STDERR,          2
+## Common status codes
+.equ END_OF_FILE,     0
+
